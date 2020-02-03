9ada149baaae1800e5258266aa812fdbc51c9031
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-02-03 16:30:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,17 @@ var taro = new Vue({
       },
       yaku_mitsuru:function(){
         if(this.sai1==1 && this.sai2==1 && this.sai3==1){
-            
+          this.msg="ピンゾロ"
+        }else if(this.sai1==2 && this.sai2==2 && this.sai3==2){
+          this.msg="アラシ"
+        }else if(this.sai1==3 && this.sai2==3 && this.sai3==3){
+          this.msg="アラシ"
+        }else if(this.sai1==4 && this.sai2==4 && this.sai3==4){
+          this.msg="アラシ"
+        }else if(this.sai1==5 && this.sai2==5 && this.sai3==5){
+          this.msg="アラシ"
+        }else if(this.sai1==6 && this.sai2==6 && this.sai3==6){
+          this.msg="アラシ"
         }else{
           this.msg="役なし"
         }
